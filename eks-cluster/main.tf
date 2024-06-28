f880fbccae40529dcd43ad8093b41eaeea913f24
michx <io@io.com>
2024-06-28 14:39:56 +0200

    reduced node groups

--- eks-cluster/main.tf
+++ eks-cluster/main.tf
@@ -83,15 +83,6 @@ module "eks" {
       desired_size = 2
     }
 
-    two = {
-      name = "node-group-2"
-
-      instance_types = ["t3.small"]
-
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
-    }
   }
 }
 
