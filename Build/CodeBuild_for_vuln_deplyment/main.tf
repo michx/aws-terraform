9a6cde8213519c1a8af5fee63e28f6021f78a5f8
michx <michele_didedda@hotmail.com>
2024-07-15 18:04:54 +0200

    Update main.tf

--- Build/CodeBuild_for_vuln_deplyment/main.tf
+++ Build/CodeBuild_for_vuln_deplyment/main.tf
@@ -96,7 +96,7 @@ data "aws_iam_policy_document" "policy_in_eks_user" {
 
 resource "aws_iam_role_policy_attachment" "role_for_eks_user" {
   role       = "eks_user_role"
-  policy_arn = data.aws_iam_policy_document.policy_in_eks_user
+  policy_arn = data.aws_iam_policy_document.policy_in_eks_user.arn
 }
 
 resource "aws_iam_role_policy" "codebuild_role_policy" {
