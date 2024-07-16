71af6367379c7f87a25cf3aef558748c17a86bec
michx <io@io.com>
2024-07-16 10:40:23 +0200

    bug correction #1

--- Build/CodeBuild_for_vuln_deplyment/main.tf
+++ Build/CodeBuild_for_vuln_deplyment/main.tf
@@ -91,10 +91,15 @@ data "aws_iam_policy_document" "policy_document_in_eks_user" {
         }
 }
 
+resource "aws_iam_policy" "policy_in_eks_user" {
+  name        = "test-policy"
+  description = "A test policy"
+  policy      = data.aws_iam_policy_document.policy_document_in_eks_user.json
+}
 
 resource "aws_iam_role_policy_attachment" "role_for_eks_user" {
   role       = eks_user_role
-  policy_arn = data.aws_iam_policy_document.policy_document_in_eks_user.arn
+  policy_arn = aws_iam_policy.policy_in_eks_user.arn
 }
 
 resource "aws_iam_role_policy" "codebuild_role_policy" {
