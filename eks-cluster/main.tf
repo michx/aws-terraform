f6ea8bda478a127ceaa54d13c7067d7c4a333059
michx <io@io.com>
2024-07-16 20:02:24 +0200

    bug correction #1

--- eks-cluster/main.tf
+++ eks-cluster/main.tf
@@ -123,8 +123,7 @@ module "eks" {
         example = {
           policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
           access_scope = {
-            namespaces = ["*"]
-            type       = "namespace"
+            type       = "cluster"
           }
         }
       }
